a7d73765c3d5ce2b2d28d96f2ab25d8f9066bc4d
eCabral87 <gcabral@u2py.mx>
2021-07-20 15:09:24 -0700

    Add simple search script

---
+++ simple_search/simple_search.py
@@ -0,0 +1,15 @@
+
+def simple_search(arr, item):
+# Inputs: arr (1xN)
+#      : item (value to find in arr)
+# Output: index of item (1xN)
+    for i in range(len(arr)):
+        if arr[i] == item:
+            return i
+    return None
+
+
+if __name__ == "__main__":
+    n = 1000
+    item = 566
+    print(simple_search([i+1 for i in range(n)], item))
